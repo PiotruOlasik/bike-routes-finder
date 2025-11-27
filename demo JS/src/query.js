6ceb987007042051b0364816e1e6838284a8ed23
PiotruOlasik <polasik01@gmail.com>
2025-11-27 13:37:00 +0100

    Zmiany na temat wyświetlania

--- demo JS/src/query.js
+++ demo JS/src/query.js
@@ -2,7 +2,7 @@ export const overpassUrl = 'https://overpass-api.de/api/interpreter';
 
 export const query = `
 [out:json][timeout:25];
-area["name"="Toruń"][admin_level=8];
+area["name"="powiat piotrkowski"][admin_level=6];
 (
   way["highway"~"cycleway|path|footway|residential|service|track|living_street"](area);
 );
